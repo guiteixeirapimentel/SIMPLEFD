5fc87345027b5574612cad624605206b2427b305
Guilherme Teixeira Pimentel <gui-teixeirapimentel@hotmail.com>
2019-11-09 15:16:14 -0300

    Thinks

---
+++ src/FlatPlaneDomain.cpp
@@ -0,0 +1,19 @@
+#include "FlatPlaneDomain.h"
+
+FlatPlaneDomain::FlatPlaneDomain(double flatPlateThickness, double flatPlateLength, double freestreamU, double freestreamV, double domainHeight, double domainLength,
+		double dx, double dy)
+:
+cNNodesHor(),
+cNNodesVer(),
+cTopLimit(),
+cBotStart(),
+cLeftLimit(),
+cRightStart(),
+cFlatPlaneBottom(),
+cFlatPlaneRight()
+{
+    h/dh + 1
+
+    h/dh 
+    v/dv
+}
