7cfd6781ca517e9d57dc056355fda30762cffe89
Guilherme Teixeira Pimentel <gui-teixeirapimentel@hotmail.com>
2019-11-10 10:25:02 -0300

    Commited

--- src/FlatPlaneDomain.h
+++ src/FlatPlaneDomain.h
@@ -26,35 +26,21 @@ public:
 	const size_t cFlatPlaneBottom;
 	const size_t cFlatPlaneRight;
 	
-	std::vector<InternalNode*> cInternalPNodesLeft;
-	std::vector<InternalNode*> cInternalPNodesTop;
-	std::vector<InternalNode*> cInternalPNodesBottom;
-	std::vector<InternalNode*> cInternalPNodesRight;
+	std::vector<InternalNode*> cInternalNodesLeft;
+	std::vector<InternalNode*> cInternalNodesTop;
+	std::vector<InternalNode*> cInternalNodesBottom;
+	std::vector<InternalNode*> cInternalNodesRight;
 
-	std::vector<InternalNode*> cInternalVNodesLeft;
-	std::vector<InternalNode*> cInternalVNodesTop;
-	std::vector<InternalNode*> cInternalVNodesBottom;
-	std::vector<InternalNode*> cInternalVNodesRight;
+	std::vector<NorthBoundary*> cFreestreamTop;
+	std::vector<SouthBoundary*> cFreestreamBottom;
+	std::vector<EastBoundary*> cFreestreamLeft;
+	std::vector<WestBoundary*> cFreestreamRight;
 
-	std::vector<NorthBoundary*> cFreestreamBPressureTop;
-	std::vector<SouthBoundary*> cFreestreamBPressureBottom;
-	std::vector<EastBoundary*> cFreestreamBPressureLeft;
-	std::vector<WestBoundary*> cFreestreamBPressureRight;
+	std::vector<WestBoundary*> cFlatPlaneEastBoundary;
+	std::vector<EastBoundary*> cFlatPlaneWestBoundary;
+	std::vector<SouthBoundary*> cFlatPlaneNorthBoundary;
+	std::vector<NorthBoundary*> cFlatPlaneSouthBoundary;
 
-	std::vector<NorthBoundary*> cFreestreamVPressureTop;
-	std::vector<SouthBoundary*> cFreestreamVPressureBottom;
-	std::vector<EastBoundary*> cFreestreamVPressureLeft;
-	std::vector<WestBoundary*> cFreestreamVPressureRight;
-
-	std::vector<WestBoundary*> cFlatPlanePEastBoundary;
-	std::vector<EastBoundary*> cFlatPlanePWestBoundary;
-	std::vector<SouthBoundary*> cFlatPlanePNorthBoundary;
-	std::vector<NorthBoundary*> cFlatPlanePSouthBoundary;
-
-	std::vector<WestBoundary*> cFlatPlaneVEastBoundary;
-	std::vector<EastBoundary*> cFlatPlaneVWestBoundary;
-	std::vector<SouthBoundary*> cFlatPlaneVNorthBoundary;
-	std::vector<NorthBoundary*> cFlatPlaneVSouthBoundary;
 };
 
 #endif
